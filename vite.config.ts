0583cee1bdc41d599b385a4465f83bc90e4e7b86
Nationayouthalliance <nationalyouthalliance.genz@gmail.com>
2026-01-08 21:22:50 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 import history from 'connect-history-api-fallback';
 import type { Plugin } from 'vite';
 
@@ -28,7 +27,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  plugins: [react(), spa404Plugin(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
